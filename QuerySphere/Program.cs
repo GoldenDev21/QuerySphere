47a254eb0c8791081501a53c8eca8ead44191937
Administrator <Administrator@EC2AMAZ-FCQ71Q7>
2024-02-29 06:36:07 +0000

    Add project files.

---
+++ QuerySphere/Program.cs
@@ -0,0 +1,27 @@
+using QuerySphere.data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<QueryShpereDbContext>();
+ 
+
+var app = builder.Build();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.MapGet("api/autos", async ([FromServices] QueryShpereDbContext db) => {
+    return await db.;
+});
+
+app.Run();
