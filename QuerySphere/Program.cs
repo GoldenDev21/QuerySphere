431c90ec1d966d4d6837f6c3a90e568f24d36db0
goldendev21 <goldendev21@outlook.com>
2024-02-29 09:22:14 +0000

    Updated to Azure key vault for connection

--- QuerySphere/Program.cs
+++ QuerySphere/Program.cs
@@ -1,6 +1,8 @@
 using QuerySphere.data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var connString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<QueryShpereDbContext>(o => o.UseSqlServer(connString));
+//var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<QueryShpereDbContext>(o => o.UseSqlServer(connString));
+
+var keyVaultEndpoint = new Uri(builder.Configuration["VaultKey"]);
+var secretClient = new SecretClient(keyVaultEndpoint, new DefaultAzureCredential());
+
+KeyVaultSecret kvs = secretClient.GetSecret("querysphereapikyvalut");
+builder.Services.AddDbContext<QueryShpereDbContext>(o => o.UseSqlServer(kvs.Value));
+
 
 var app = builder.Build();
 
